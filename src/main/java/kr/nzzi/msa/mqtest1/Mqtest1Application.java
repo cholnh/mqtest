666a54261f7c94f9006f6f861a4b5c3718c45563
Nzzi <cholnh1@naver.com>
2021-07-26 11:46:54 +0900

    Initial commit

---
+++ src/main/java/kr/nzzi/msa/mqtest1/Mqtest1Application.java
@@ -0,0 +1,13 @@
+package kr.nzzi.msa.mqtest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mqtest1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mqtest1Application.class, args);
+    }
+
+}
---
+++ src/test/java/kr/nzzi/msa/mqtest1/Mqtest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.nzzi.msa.mqtest1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mqtest1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
