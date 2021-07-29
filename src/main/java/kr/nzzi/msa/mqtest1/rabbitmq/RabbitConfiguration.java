3d0a7b250f7ba42837a8ce34eb8d2b645fb8796e
Nzzi <cholnh1@naver.com>
2021-07-29 18:31:58 +0900

    edit exchange type - topic 으로 변경

--- src/main/java/kr/nzzi/msa/mqtest1/rabbitmq/RabbitConfiguration.java
+++ src/main/java/kr/nzzi/msa/mqtest1/rabbitmq/RabbitConfiguration.java
@@ -1,8 +1,7 @@
 package kr.nzzi.msa.mqtest1.rabbitmq;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.config.RetryInterceptorBuilder;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
@@ -16,10 +15,38 @@ import org.springframework.context.annotation.Configuration;
 @RequiredArgsConstructor
 public class RabbitConfiguration {
 
+    private final String PRODUCT_EXCHANGE = "product.exchange";
+
     private final int MAX_TRY_COUNT = 3;
     private final int INITIAL_INTERVAL = 3000;
     private final int MULTIPLIER = 3;
     private final int MAX_INTERVAL = 10000;
+
+    @Bean
+    TopicExchange productMessageExchange() {
+        return ExchangeBuilder
+                .topicExchange(PRODUCT_EXCHANGE)
+                .durable(true)
+                .build();
+    }
+
+    @Bean
+    TopicExchange productSaveExchange() {
+        return ExchangeBuilder
+                .topicExchange(PRODUCT_EXCHANGE)
+                .durable(true)
+                .build();
+    }
+
+    @Bean
+    Binding productMessageBinding() {
+        return BindingBuilder.bind(productMessageEvent()).to(productMessageExchange()).with(RabbitMqEvent.PRODUCT_MESSAGE_EVENT);
+    }
+
+    @Bean
+    Binding productSaveBinding() {
+        return BindingBuilder.bind(productSaveEvent()).to(productSaveExchange()).with(RabbitMqEvent.PRODUCT_SAVE_EVENT);
+    }
 
     @Bean
     public Queue productMessageEvent() {
@@ -31,7 +58,7 @@ public class RabbitConfiguration {
     @Bean
     public Queue productSaveEvent() {
         return QueueBuilder
-                .durable(RabbitMqEvent.PRODUCT_SAVE_EVENT)
+                .durable(RabbitMqEvent.PRODUCT_SAVE_EVENT )
                 .build();
     }
 
--- src/main/java/kr/nzzi/msa/mqtest1/rabbitmq/RabbitMqEvent.java
+++ src/main/java/kr/nzzi/msa/mqtest1/rabbitmq/RabbitMqEvent.java
@@ -1,6 +1,6 @@
 package kr.nzzi.msa.mqtest1.rabbitmq;
 
 public interface RabbitMqEvent {
-    String PRODUCT_MESSAGE_EVENT = "product.message";
-    String PRODUCT_SAVE_EVENT = "product.save";
+    String PRODUCT_MESSAGE_EVENT = "product.message.mqtest1";
+    String PRODUCT_SAVE_EVENT = "product.save.mqtest1";
 }
